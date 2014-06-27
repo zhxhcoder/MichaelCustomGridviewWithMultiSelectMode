1b699cf7264070d5f9a9e0aa924620f89683ad53
michael <zhuxiaohui001@163.com>
2014-06-27 18:07:21 +0800

    michael commit

--- src/com/michael/HomeActivity.java
+++ src/com/michael/HomeActivity.java
@@ -25,40 +25,10 @@ public class HomeActivity extends Activity implements MultiChoiceModeListener {
     private Map<Integer, Boolean> mSelectMap = new HashMap<Integer, Boolean>();
 
     private List<Integer> mImgIdList;
-
     private Integer[] mImgIds = new Integer[]{R.drawable.img_1, R.drawable.img_2,
             R.drawable.img_3, R.drawable.img_4, R.drawable.img_5,
-            R.drawable.img_6, R.drawable.img_7, R.drawable.img_8,
-            R.drawable.img_9, R.drawable.img_1, R.drawable.img_2,
-            R.drawable.img_3, R.drawable.img_4, R.drawable.img_5,
-            R.drawable.img_6, R.drawable.img_7, R.drawable.img_3,
-            R.drawable.img_4, R.drawable.img_5, R.drawable.img_5,
-            R.drawable.img_1, R.drawable.img_2,
-            R.drawable.img_3, R.drawable.img_4, R.drawable.img_5,
-            R.drawable.img_6, R.drawable.img_7, R.drawable.img_8,
-            R.drawable.img_9, R.drawable.img_1, R.drawable.img_2,
-            R.drawable.img_3, R.drawable.img_4, R.drawable.img_5,
-            R.drawable.img_6, R.drawable.img_7, R.drawable.img_3,
-            R.drawable.img_4, R.drawable.img_5, R.drawable.img_5,
-            R.drawable.img_1, R.drawable.img_2,
-            R.drawable.img_3, R.drawable.img_4, R.drawable.img_5,
-            R.drawable.img_6, R.drawable.img_7, R.drawable.img_8,
-            R.drawable.img_9, R.drawable.img_1, R.drawable.img_2,
-            R.drawable.img_3, R.drawable.img_4, R.drawable.img_5,
-            R.drawable.img_6, R.drawable.img_7, R.drawable.img_3,
-            R.drawable.img_4, R.drawable.img_5, R.drawable.img_5,
-            R.drawable.img_1, R.drawable.img_2,
-            R.drawable.img_3, R.drawable.img_4, R.drawable.img_5,
-            R.drawable.img_6, R.drawable.img_7, R.drawable.img_8,
-            R.drawable.img_9, R.drawable.img_1, R.drawable.img_2,
-            R.drawable.img_3, R.drawable.img_4, R.drawable.img_5,
-            R.drawable.img_6, R.drawable.img_7, R.drawable.img_3,
-            R.drawable.img_3, R.drawable.img_4, R.drawable.img_5,
-            R.drawable.img_6, R.drawable.img_7, R.drawable.img_8,
-            R.drawable.img_9, R.drawable.img_1, R.drawable.img_2,
-            R.drawable.img_3, R.drawable.img_4, R.drawable.img_5,
-            R.drawable.img_6, R.drawable.img_7, R.drawable.img_3,
-            R.drawable.img_4, R.drawable.img_5, R.drawable.img_5};
+            R.drawable.img_6, R.drawable.img_7, R.drawable.img_8
+};
 
     /**
      * Called when the activity is first created.
